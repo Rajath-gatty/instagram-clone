c898d3b738f2e968e54c8bdb5ac8513f36605357
Rajath-gatty <rajathgatty253@gmail.com>
2023-04-26 10:26:07 +0530

    homepage bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Posts from "./Components/Posts";
 import getUserPosts from "./libs/getUserPosts";
 import { homePost } from "./types";
 
-const HomePage = async({session}:{session:any}) => {
+const HomePage = async() => {
   const posts: homePost[] = await getUserPosts();
   return (
       <div className="p-3">
